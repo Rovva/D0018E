80e46f5a444291e8da43eb682ffb76c1f8f24bb5
Rovva <rovva@hostarn.se>
2017-11-25 16:51:03 +0100

    Made rudimentary design and function on adminpages

---
+++ admin_categories.php
@@ -0,0 +1,120 @@
+<?php
+    $category = "d0018e_categories";
+    $orders = "d0018e_orders";
+    $products = "d0018e_products";
+    $carts = "d0018e_carts";
+    
+    $manufacturer = "d0018e_manufacturers";
+    
+    $dbhost = "localhost";
+    $dbname = "skola";
+    $dbusr = "skola";
+    $dbpass = "skola";
+    global $conn;
+    $conn = mysql_connect($dbhost, $dbusr, $dbpass);
+    mysql_select_db($dbname);
+    mysql_set_charset("utf8", $conn);
+    if (!$conn) { 
+        die('Could not establish a connection: ' . mysql_error());
+    }
+?>
+<!DOCTYPE html>
+<html>
+<title>Adminpage</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="dropdown.css">
+<body>
+
+<div class="main">
+
+	<div class="banner">
+	</div>
+	
+    <div class="navigationbar">
+    
+        <div class="navigationbar_dropdowns">
+        
+            <div class="dropdown">
+              <button class="firstdropbtn"><a class="firstdropbtn" href="admin_index.html">Home</a></button>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn">Manage site</button>
+              <div class="dropdown-content">
+                <a href="admin_categories.html">Categories</a>
+                <a href="admin_manufacturers.html">Manufacturers</a>
+                <a href="admin_products.html">Products</a>
+                <a href="admin_images.html">Images</a>
+              </div>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn"><a class="dropbtn" href="admin_users.html">Manage users</a></button>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn"><a class="dropbtn" href="admin_logout.html">Logout</a></button>
+            </div>
+        
+        </div>        
+    </div>
+    
+	<div class="mainbody">
+    
+        <div class="mainborders">
+        
+           <p class="mainbody_text">
+           
+            <table style="width: 100%;">
+                <tr>
+                    <th style="text-align: left; width: 50px;">Id</th>
+                    <th style="text-align: left; width: 150px;">Name</th>
+                    <th style="text-align: left; width: 50px;">Products</th>
+                    <th style="width: 20px;"></th>
+                    <th style="text-align: left; width: 150px;"></th>
+                    <th style="text-align: left;"></th>
+                </tr>
+                <?php
+                    
+                    $sql = "SELECT * FROM $categories";
+                    $query = mysql_query($sql);
+                    
+                    while($row = mysql_fetch_array($query)) {
+                        echo '                <tr>
+                    <form action="admin_categories.php">
+                        <input type="hidden" name="id" value="' . $row['id'] . '">
+                        <th style="text-align: left;">' . $row['id'] . '</th>
+                        <th style="text-align: left;">' . $row['name'] . '</th>
+                        <th style="text-align: left;">' . $row['num_Products'] . '</th>
+                        <th></th>
+                        <th style="text-align: left;"><input type="button" name="edit" value="Edit category"></th>
+                        <th style="text-align: left;"><input type="button" name="remove" value="Remove category"></th>
+                    </form>
+                </tr>';
+                    }
+                ?>
+                    <tr>
+                        <form action="admin_categories.php">
+                        <th><input type="button" name="add" value="Add category"></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+            </table>
+            
+	   </p>
+        
+        </div>
+    
+	</div>
+	
+	<div class="footer">
+		<p class="copyright_text"> Christoffer Rova & Simon Herbertsson &copy; 2017</p>
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ admin_products.php
@@ -0,0 +1,143 @@
+<?php
+    $category = "d0018e_categories";
+    $orders = "d0018e_orders";
+    $products = "d0018e_products";
+    $carts = "d0018e_carts";
+    
+    $manufacturer = "d0018e_manufacturers";
+    
+    $dbhost = "localhost";
+    $dbname = "skola";
+    $dbusr = "skola";
+    $dbpass = "skola";
+    global $conn;
+    $conn = mysql_connect($dbhost, $dbusr, $dbpass);
+    mysql_select_db($dbname);
+    mysql_set_charset("utf8", $conn);
+    if (!$conn) { 
+        die('Could not establish a connection: ' . mysql_error());
+    }
+?>
+<!DOCTYPE html>
+<html>
+<title>Adminpage</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="dropdown.css">
+<body>
+
+<div class="main">
+
+	<div class="banner">
+	</div>
+	
+    <div class="navigationbar">
+    
+        <div class="navigationbar_dropdowns">
+        
+            <div class="dropdown">
+              <button class="firstdropbtn"><a class="firstdropbtn" href="admin_index.html">Home</a></button>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn">Manage site</button>
+              <div class="dropdown-content">
+                <a href="admin_categories.html">Categories</a>
+                <a href="admin_manufacturers.html">Manufacturers</a>
+                <a href="admin_products.html">Products</a>
+                <a href="admin_images.html">Images</a>
+              </div>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn"><a class="dropbtn" href="admin_users.html">Manage users</a></button>
+            </div>
+            
+            <div class="dropdown">
+              <button class="dropbtn"><a class="dropbtn" href="admin_logout.html">Logout</a></button>
+            </div>
+        
+        </div>        
+    </div>
+    
+	<div class="mainbody">
+    
+        <div class="mainborders">
+        
+           <p class="mainbody_text">
+           
+            <table style="width: 100%;">
+                <tr>
+                    <th style="text-align: left; width: 50px;">Id</th>
+                    <th style="text-align: left; width: 50px;">Manufacturer</th>
+                    <th style="text-align: left; width: 150px;">Name</th>
+                    <th style="text-align: left; width: 50px;">Cost</th>
+                    <th style="text-align: left; width: 50px;">Stock</th>
+                    <th style="width: 20px;"></th>
+                    <th style="text-align: left; width: 150px;"></th>
+                    <th style="text-align: left; width: 150px;"></th>
+                    <th style="text-align: left;"></th>
+                </tr>
+                <?php
+                    
+                    $sql = "SELECT $products.id, $manufacturer.name AS manufacturername, $products.name, 
+                    $products.cost, $products.stock, $products.manufacturer 
+                    FROM $products 
+                    LEFT JOIN $manufacturer ON $products.manufacturer = $manufacturer.id";
+                    
+                    $query = mysql_query($sql);
+                    while($row = mysql_fetch_array($query)) {
+                        echo '                <tr>
+                    <form action="admin_products.php">
+                        <input type="hidden" name="id" value="' . $row['id'] . '">
+                        <th style="text-align: left;">' . $row['id'] . '</th>
+                        <th style="text-align: left;">' . $row['manufacturername'] . '</th>
+                        <th style="text-align: left;">' . $row['name'] . '</th>
+                        <th style="text-align: left;">' . $row['cost'] . ':-</th>
+                        <th style="text-align: left;">' . $row['stock'] . '</th>
+                        <th></th>
+                        <th style="text-align: left;"><input type="button" name="view" value="View product"></th>
+                        <th style="text-align: left;"><input type="button" name="edit" value="Edit product"></th>
+                        <th style="text-align: left;"><input type="button" name="remove" value="Remove product"></th>
+                    </form>
+                </tr>';
+                    }
+                ?>
+                <tr style="height: 20px;">
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <form action="admin_products.php">
+                    <th><input type="button" name="add" value="Add product"></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th style="width: 20px;"></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    </form>
+                </tr>
+            </table>
+            
+            
+	   </p>
+        
+        </div>
+    
+	</div>
+	
+	<div class="footer">
+		<p class="copyright_text"> Christoffer Rova & Simon Herbertsson &copy; 2017</p>
+	</div>
+
+</div>
+
+</body>
+</html>
