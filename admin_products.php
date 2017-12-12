621a45aeab5affb489e40f30ba7133d73bba4755
Rovva <rovva@hostarn.se>
2017-12-12 11:25:26 +0100

    Cleaned up old files and removed html code from admin phps

--- admin_products.php
+++ admin_products.php
@@ -331,53 +331,10 @@
             list_items($table, $table2, $mysqli);
         }
     }
-    
+
+    include("admin_html/admin_upper.html");
+    include("admin_html/admin_menu.html");
+    include("admin_html/admin_middle.html");
+    select_function($products, $manufacturer, $mysqli);
+    include("admin_html/admin_bottom.html");
 ?>
-<!DOCTYPE html>
-<html>
-<title>Adminpage</title>
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="dropdown.css">
-<body>
-
-<div class="main">
-
-	<div class="banner">
-	</div>
-	
-    <div class="navigationbar">
-    
-        <div class="navigationbar_dropdowns">
-        
-        <?php
-        include("admin_menu.html");
-        ?>
-        
-        </div>        
-    </div>
-    
-	<div class="mainbody">
-    
-        <div class="mainborders">
-        
-           <p class="mainbody_text">
-           
-                <?php
-                    select_function($products, $manufacturer, $mysqli);
-                ?>
-            
-            
-	   </p>
-        
-        </div>
-    
-	</div>
-	
-	<div class="footer">
-		<p class="copyright_text"> Christoffer Rova & Simon Herbertsson &copy; 2017</p>
-	</div>
-
-</div>
-
-</body>
-</html>
--- admin_users.php
+++ admin_users.php
@@ -332,52 +332,9 @@
         }
     }
 
+    include("admin_html/admin_upper.html");
+    include("admin_html/admin_menu.html");
+    include("admin_html/admin_middle.html");
+    select_function($users, $mysqli);
+    include("admin_html/admin_bottom.html");
 ?>
-<!DOCTYPE html>
-<html>
-<title>Adminpage</title>
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="dropdown.css">
-<body>
-
-<div class="main">
-
-	<div class="banner">
-	</div>
-	
-    <div class="navigationbar">
-    
-        <div class="navigationbar_dropdowns">
-        
-        <?php
-        include("admin_menu.html");
-        ?>
-        
-        </div>        
-    </div>
-    
-	<div class="mainbody">
-    
-        <div class="mainborders">
-        
-           <p class="mainbody_text">
-           
-                <?php
-                    select_function($users, $mysqli);
-                ?>
-            
-            
-	   </p>
-        
-        </div>
-    
-	</div>
-	
-	<div class="footer">
-		<p class="copyright_text"> Christoffer Rova & Simon Herbertsson &copy; 2017</p>
-	</div>
-
-</div>
-
-</body>
-</html>
