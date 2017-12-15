41a7a50888e8dde525010503235de271a78f1807
Rovva <rovva@hostarn.se>
2017-12-15 20:06:17 +0100

    Userpage almost done

--- final_site/user_page.php
+++ final_site/user_page.php
@@ -2,47 +2,133 @@
 
     include("db.php");
 
+    function list_info($table, $mysqli) {
+                
+        $id = $_SESSION['user_id'];
+        
+        $stmt = $mysqli->prepare("SELECT email, fName, sName, address, city, postcode, state, country, phone FROM $table WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+    	$stmt->store_result();
+    	$stmt->bind_result($email, $fName, $sName, $address, $city, $postcode, $state, $country, $phone);
+    	$stmt->fetch();
+        
+        echo '
+        <table>
+            <tr>
+                <th colspan="2">Your personal information:</th>
+            </tr>
+            <tr>
+                <th style="text-align: left;">Name:</th>
+                <th style="text-align: left;">' . $fName . " " . $sName . '</th>
+            </tr>
+            <tr>
+                <th style="text-align: left;">Email:</th>
+                <th style="text-align: left;">' . $email . '</th>
+            </tr>
+            <tr>
+                <th style="text-align: left;">Address:</th>
+                <th style="text-align: left;">' . $address . '<br>
+                    ' . $postcode . ' ' . $city . '<br>
+                    ' . $state . ' ' . $country . '
+                </th>
+            </tr>
+            <tr>
+                <th style="text-align: left;">Phone:</th>
+                <th style="text-align: left;">' . $phone . '</th>
+            </tr>
+            <tr>
+                <th colspan="2" style="text-align: center;"><a href="user_page.php?action=edit_info">Edit information</a></th>
+            </tr>
+        </table>';
+    }
+    
+    function edit_info($table, $mysqli) {
+        $id = $_SESSION['user_id'];
+        
+        $stmt = $mysqli->prepare("SELECT email, fName, sName, address, city, postcode, state, country FROM $table WHERE id = ?");
+        
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+    	$stmt->store_result();
+    	$stmt->bind_result($email, $fName, $sName, $address, $city, $postcode, $state, $country);
+    	$stmt->fetch();
+        
+        echo '
+        <form method="post" action="user_page.php">
+        First name:&nbsp;<input type="text" name="fname" value="' . $fName . '"><br>
+        Last name:&nbsp;<input type="text" name="sname" value="' . $sName . '"><br>
+        Email:&nbsp;<input type="text" name="email" value="' . $email . '"><br>
+        Phone:&nbsp;<input type="text" name="phone" value="' . $phone . '"><br>
+        Address:&nbsp;<input type="text" name="address" value="' . $address . '"><br>
+        Postcode:&nbsp;<input type="text" name="postcode" value="' . $postcode . '"><br>
+        City:&nbsp;<input type="text" name="city" value="' . $city . '"><br>
+        State:&nbsp;<input type="text" name="state" value="' . $state . '"><br>
+        Country:&nbsp;<input type="text" name="country" value="' . $country . '"><br>
+        <input type="submit" name="save_info" value="Save">';
+    }
+
+    function save_info($table, $mysqli) {
+        $fname = $_POST['fname'];
+        $sname = $_POST['sname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $postcode = $_POST['postcode'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $country = $_POST['country'];
+        
+        $id = $_SESSION['user_id'];
+        
+        $stmt = $mysqli->prepare("UPDATE $table SET fName = ?, sName = ?, email = ?, phone = ?,
+         address = ?, postcode = ?, city = ?, state = ?, country = ? WHERE id = ?");
+        $stmt->bind_param("sssisisssi", $fname, $sname, $email, $phone, $address, $postcode, $city, $state, $country, $id);
+        $stmt->execute();
+        
+        if(!$stmt) {
+            echo 'Something went wrong: ' . $mysqli->error;
+        } else {
+            echo 'Your updated information has been saved.';
+        }
+        
+    }
+    
+    function list_orders() {
+        
+    }
+    
+    function view_order() {
+        
+    }
+    
+    function change_password() {
+        
+    }
+    
+    function save_new_password() {
+        
+    }
+    
+    function select_function($table, $mysqli) {
+        $action = isset($_GET['action']);
+        
+        $save_info = isset($_POST['save_info']);
+        
+        if($action == "edit_info") {
+            edit_info($table, $mysqli);
+        } else if ($save_info) {
+            save_info($table, $mysqli);
+        } else {
+            list_info($table, $mysqli);
+        }
+    }
+    
     include("store_html/top.html");
     include("store_html/menu.html");
     include("store_html/middle.html");
-    
-    $id = $_SESSION['user_id'];
-    
-    $stmt = $mysqli->prepare("SELECT email, fName, sName, address, city, postcode, state, country FROM $users WHERE id = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($email, $fName, $sName, $address, $city, $postcode, $state, $country);
-	$stmt->fetch();
-    
-    echo '
-<table>
-    <tr>
-        <th colspan="2">Your personal information:</th>
-    </tr>
-    <tr>
-        <th style="text-align: left;">Name:</th>
-        <th style="text-align: left;">' . $fName . " " . $sName . '</th>
-    </tr>
-    <tr>
-        <th style="text-align: left;">Email:</th>
-        <th style="text-align: left;">' . $email . '</th>
-    </tr>
-    <tr>
-        <th style="text-align: left;">Address:</th>
-        <th style="text-align: left;">' . $address . '<br>
-            ' . $postcode . ' ' . $city . '<br>
-            ' . $state . ' ' . $country . '
-        </th>
-    </tr>
-    <tr>
-        <th style="text-align: left;">Phone:</th>
-        <th style="text-align: left;">' . $phone . '</th>
-    </tr>
-    <tr>
-        <th colspan="2" style="text-align: center;"><a href="">Edit information</a></th>
-    </tr>
-</table>';
+
+    select_function($users, $mysqli);
 
     include("store_html/bottom.html");
 ?>
---
+++ final_site/user_review.php
@@ -0,0 +1,17 @@
+<?php
+
+    include("db.php");
+    
+    function list_review() {
+        
+    }
+    
+    function make_review() {
+        
+    }
+    
+    function save_review() {
+        
+    }
+
+?>
